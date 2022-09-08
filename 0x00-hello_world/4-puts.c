5c3a8fb3e063e80e89960f9b093e0047b4f7edc1
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-08 10:36:42 -0700

    Four

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,30 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+   /**
+
+    * main - Program entry point
+
+    *
+
+    * Return: 0 if successful, 1 otherwise
+
+    */
+
+int main(void)
+
+{
+  
+  char text[52] = "\"Programming is like building a multilingual puzzle";
+  
+
+  
+  puts(text);
+  
+
+  
+  return (0);
+  
+}
