d5de948a275631ee2d29a3611054d0f529ea93bd
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-08 13:48:01 -0700

    Four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,10 @@
+(setq c-default-style "bsd"
+ 
+      c-basic-offset 8
+ 
+      tab-width 8
+ 
+ indent-tabs-mode t)
 #include <stdio.h>
 
 /**
@@ -7,9 +14,7 @@
  **/
 int main(void)
 {
- char text[52] = "\"Programming is like building a multilingual puzzle";
-
- puts(text);
-
- return (0);
+  char text[52] = "\"Programming is like building a multilingual puzzle";
+  puts(text);
+  return (0);
 }
