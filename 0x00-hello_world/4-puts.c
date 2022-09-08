7e72d67602cef00bdf19123d4018cb7a7e4cf0bc
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-08 14:16:38 -0700

    Four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
-(setq c-default-style "bsd"
- 
-      c-basic-offset 8
- 
-      tab-width 8
- 
- indent-tabs-mode t)
 #include <stdio.h>
 
 /**
  * main - Program entry point
- *
+ * 
  * Return: 0 if successful, 1 otherwise
  **/
 int main(void)
 {
-  char text[52] = "\"Programming is like building a multilingual puzzle";
-  puts(text);
-  return (0);
+          char text[52] = "\"Programming is like building a multilingual puzzle";
+
+	  puts(text);
+
+	  return (0);
 }
