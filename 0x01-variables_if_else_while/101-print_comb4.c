15e09ff175ae52fc2eb725ad8a81e2fedd46b3c5
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-09 06:48:11 -0700

    Advanced2

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.>
+
+/**
+ * main - Program entry point. Prints possible combination of double digits
+ *
+ 8 Return: 0 if successful, 1 otherwise
+*/
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = i + 1; j <= 56; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == 55 && j == 56 && k == 57)
+					break;
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
