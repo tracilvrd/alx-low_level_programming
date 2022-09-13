37dad62829dc1d388c8401231e1859869ac64501
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-13 13:56:17 -0700

    Advanced2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* Function Declaration */
+int sum_naturals_3_5(int num);
+
+/**
+ * main - Program entry point.
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int result = sum_naturals_3_5(1024);
+
+	printf("%d\n", result);
+
+	return (0);
+}
+
+/**
+ * sum_naturals_3_5 - Sums all the natural numbers divisible by 3 or 5,
+ * and below a number(excluded).
+ *
+ * @num: Number at which to stop summation.
+ *
+ * Return: total sum of naturals.
+ */
+int sum_naturals_3_5(int num)
+{
+	int count, sum = 0;
+
+	for (count = 3; count < num; count++)
+	{
+		if (count % 3 == 0 || count % 5 == 0)
+			sum += count;
+	}
+
+	return (sum);
+}
