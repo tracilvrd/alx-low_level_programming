74d7f1891a946b2b480f57735c7f931399e70595
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-13 04:23:41 -0700

    One

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet. Prints lowercase alphabets
+ *
+ * Return: none
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar(10);
+}
+
+	      
+  
