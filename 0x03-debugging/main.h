93a8052bd2f9d450ce99066e2dc9ea35f949434b
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-14 06:35:46 -0700

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 /**
  * positive_or_negative - Function Prototype.
  *
@@ -13,4 +15,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif/* MAIN_H */
+#endif /* MAIN_H */
