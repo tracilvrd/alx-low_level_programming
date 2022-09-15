ce4000864e4535cc9061c8d0bfa9387be53d3327
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-15 04:45:22 -0700

    0X04-more_functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Check for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if c is uppercase. 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c < 65 || c > 90)
+		return (0);
+	return (1);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * main - check functions
+ *
+ * Return: None
+ */
+int main(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+int fizzBuzz(int num);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif/* _MAIN_H_ */
