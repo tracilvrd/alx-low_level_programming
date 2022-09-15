3e1470ad846c5442c295943c4a21641ad2308506
tracilvrd <oghenekparobo.ese@gmail.com>
2022-09-15 08:08:25 -0700

    0x04-more_function_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers 0 to 9
+ * print_numbers - prints the numbers from 0 through to 9.
  *
- * Return: none
+ * Return: None.
  */
 void print_numbers(void)
 {
