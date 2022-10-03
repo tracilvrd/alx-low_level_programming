49f4d9419083cbe264fcc368c9d9bcc47a4edaa6
tracilvrd <oghenekparobo.ese@gmail.com>
2022-10-03 04:55:05 -0700

    0x09

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: Always 0.
+ */
 int main(void);
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -5,4 +17,5 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
+
+#endif /* _MAIN_H_ */
