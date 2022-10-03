36285be4120da68c295b6e21e68177f1b5af1cc7
tracilvrd <oghenekparobo.ese@gmail.com>
2022-10-03 02:19:41 -0700

    0x09

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +30,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _MAIN_H_ */
